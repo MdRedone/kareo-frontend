1f0e250ddb688f8fdf78d7a339fd50dbb69271f7
MdRedone <redone.hassan@sjinnovation.com>
2023-04-05 10:29:49 +0600

    worked csv fetch

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,58 +1,73 @@
 /* eslint-disable no-lone-blocks */
-import React, { useState } from "react";
-import "./Home.css";
-import { Container } from "react-bootstrap";
-import { Button, Form } from "react-bootstrap";
-import axios from "axios";
-function Home() {
-  const [file, setFile] = useState(null);
+import React, { useState } from 'react'
+import './Home.css'
+import { Container } from 'react-bootstrap'
+import { Button, Form } from 'react-bootstrap'
+import axios from 'axios'
+function Home () {
+  const [file, setFile] = useState(null)
+  console.log(file)
+  const UPLOAD_ENDPOINT = 'http://localhost:5000/api/file/create'
 
-  const UPLOAD_ENDPOINT = "http://localhost:5000/api/file/create";
+  const handleSubmit = async e => {
+    e.preventDefault()
+    await uploadFile(file)
+  }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    let res = await uploadFile(file);
-    console.log(res.data);
-  };
+  const processCSV = async (str, delim = ',') => {
+    const headers = str.slice(0, str.indexOf('\n')).split(delim)
+    const rows = str.slice(str.indexOf('\n') + 1).split('\n')
 
-  const uploadFile = async (file) => {
-    const formData = new FormData();
-    formData.append("files", file);
-    // console.log(file);
-    return await axios.post(UPLOAD_ENDPOINT, formData, {
-      headers: {
-        "content-type": "multipart/form-data",
-      },
-    });
-  };
+    const newArray = rows.map(row => {
+      const values = row.split(delim)
+      const eachObject = headers.reduce((obj, header, i) => {
+        obj[header] = values[i]
+        return obj
+      }, {})
+      return eachObject
+    })
 
-  const handleOnChange = (e) => {
-    if (e.target.files[0].type === "text/csv") {
-      setFile(e.target.files[0]);
-    } else {
-      alert("Please Enter Excel file");
+    return await axios.post(UPLOAD_ENDPOINT, newArray)
+  }
+
+  const uploadFile = async file => {
+    const reader = new FileReader()
+
+    reader.onload = function (e) {
+      const text = e.target.result
+      processCSV(text)
     }
-  };
+
+    reader.readAsText(file)
+  }
+
+  const handleOnChange = e => {
+    if (e.target.files[0].type === 'text/csv') {
+      setFile(e.target.files[0])
+    } else {
+      alert('Please Enter Excel file')
+    }
+  }
   return (
-    <div className="home mt-5 pt-2">
+    <div className='home mt-5 pt-2'>
       <br />
       <br />
-      <Container className="mt-1">
-        <Form validate="true" onSubmit={handleSubmit}>
+      <Container className='mt-1'>
+        <Form validate='true' onSubmit={handleSubmit}>
           <Form.Label>Upload File</Form.Label>
           <Form.Control
-            type="file"
+            type='file'
             onChange={handleOnChange}
             required
-            name="username"
+            name='username'
           />
-          <Button type="submit" className="btn-primary mt-2">
+          <Button type='submit' className='btn-primary mt-2'>
             Submit
           </Button>
         </Form>
       </Container>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
